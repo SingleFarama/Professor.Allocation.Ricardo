d66f707f3a2b044b1138e9fa2f4ef65f7d45f10d
Ricardo Lira da Silva <ricardoliradasilva16@gmail.com>
2022-01-28 16:54:57 -0300

    Criação das classes de Repositório

---
+++ professor-allocation-ricardo/src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.repository;
+
+import com.project.professor.allocation.entity.Allocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+
+    List<Allocation> findByCourseId(Long courseId);
+}
