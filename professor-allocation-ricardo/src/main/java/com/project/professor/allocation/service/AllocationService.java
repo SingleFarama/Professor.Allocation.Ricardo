bd33f13d995c3d8685ecd6d7cfd03fd2691021d8
Ricardo Lira da Silva <ricardoliradasilva16@gmail.com>
2022-01-28 17:02:25 -0300

    Criação das classes de Serviço

---
+++ professor-allocation-ricardo/src/main/java/com/project/professor/allocation/service/AllocationService.java
@@ -0,0 +1,24 @@
+package com.project.professor.allocation.service;
+
+import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.repository.AllocationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AllocationService {
+
+    private AllocationRepository allocationRepository;
+
+    public AllocationService(AllocationRepository allocationRepository) {
+        this.allocationRepository = allocationRepository;
+    }
+
+    public Allocation findById(Long id)
+    {
+        Optional<Allocation> allocationOptional = allocationRepository.findById(id);
+        Allocation allocation = allocationOptional.orElse(null);
+        return allocation;
+    }
+}
---
+++ professor-allocation-ricardo/src/main/java/com/project/professor/allocation/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.project.professor.allocation.service;
+
+import com.project.professor.allocation.entity.Department;
+import com.project.professor.allocation.repository.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public Department findById(Long id)
+    {
+        Optional<Department> departmentOptional = departmentRepository.findById(id);
+        Department department = departmentOptional.orElse(null);
+        return department;
+    }
+
+}
