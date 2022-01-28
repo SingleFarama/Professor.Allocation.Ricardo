be2275504a134c82de50b8b85f73bf905989b52a
SingleFarama <98427016+SingleFarama@users.noreply.github.com>
2022-01-28 15:55:42 -0300

    Add files via upload

---
+++ professor-allocation-ricardo/src/test/java/com/project/professorallocationricardo/ProfessorAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professorallocationricardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
