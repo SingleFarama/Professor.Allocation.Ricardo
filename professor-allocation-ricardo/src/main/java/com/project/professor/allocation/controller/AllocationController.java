81c22a66d0dbba38c7daea18958521ee43b66f1a
Ricardo <ricardoliradasilva16@gmail.com>
2022-02-09 21:59:26 -0300

    Atualização das classes de controle

--- professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -1,9 +1,17 @@
 package com.project.professor.allocation.controller;
 
+import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.entity.Department;
 import com.project.professor.allocation.service.AllocationService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/allocations")
 public class AllocationController {
 
     private final AllocationService allocationService;
@@ -11,4 +19,51 @@ public class AllocationController {
     public AllocationController(AllocationService allocationService) {
         this.allocationService = allocationService;
     }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Allocation>> findAll() {
+        List<Allocation> allocations= allocationService.findAll();
+        return new ResponseEntity<>(allocations, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{allocation_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Allocation> findById(@PathVariable(name = "allocation_id") Long id) {
+        Allocation allocation = allocationService.findById(id);
+        if (allocation == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(allocation, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Allocation> create(Allocation allocation) {
+        try {
+            allocation = allocationService.create(allocation);
+            return new ResponseEntity<>(allocation, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping(path = "/{department_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Allocation> update(Long id, Allocation allocation) {
+        allocation.setId(id);
+        try {
+            allocation = allocationService.update(allocation);
+            if (allocation == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<>(allocation, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    
 }
--- professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/DepartmentController.java
+++ professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/DepartmentController.java
@@ -1,14 +1,62 @@
 package com.project.professor.allocation.controller;
 
+import com.project.professor.allocation.entity.Department;
 import com.project.professor.allocation.service.DepartmentService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/departments")
 public class DepartmentController {
 
     private final DepartmentService departmentService;
 
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Department>> findAll() {
+        List<Department> departments = departmentService.findAll();
+        return new ResponseEntity<>(departments, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{department_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Department> findById(@PathVariable(name = "department_id") Long id) {
+        Department department = departmentService.findById(id);
+        if (department == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(department, HttpStatus.OK);
+        }
+    }
+
+    public ResponseEntity<Department> create(Department department) {
+        try {
+            department = departmentService.create(department);
+            return new ResponseEntity<>(department, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<Department> update(Long id, Department department) {
+        department.setId(id);
+        try {
+            department = departmentService.update(department);
+            if (department == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<>(department, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
