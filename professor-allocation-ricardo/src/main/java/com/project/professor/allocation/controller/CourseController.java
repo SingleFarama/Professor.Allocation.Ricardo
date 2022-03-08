e6cc6371fc42deee84728dd1486f29ff9f49502c
Ricardo <ricardoliradasilva16@gmail.com>
2022-03-08 20:05:37 -0300

    Modificação dos diretórios em Professor e Course

--- professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/CourseController.java
+++ professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/CourseController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/courses")
 public class CourseController {
 
     private final CourseService courseService;
--- professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/ProfessorController.java
+++ professor-allocation-ricardo/src/main/java/com/project/professor/allocation/controller/ProfessorController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/Allocations")
 public class ProfessorController {
 
     private final ProfessorService professorService;
